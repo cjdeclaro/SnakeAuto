9204ab8d2fc9ddded3605996f6517051438b0200
Brandon Mauricio <b24.mauricio@gmail.com>
2024-07-01 13:47:41 +0800

    brandon

--- src/Home.java
+++ src/Home.java
@@ -1,9 +1,9 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Home {
 	public static void main(String[] args) {
 		JFrame frm = new JFrame("Ahas ni Jalem");
-		
+		JButton jalemBtn = new JButton("btn");
 		frm.setVisible(true);
 		frm.setSize(500,500);
 		
