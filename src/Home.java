b861ac2d688ef6ee75dc001efaa29d09d3fdb40b
bryan reano <you@example.com>
2024-07-01 13:53:57 +0800

    bryan

--- src/Home.java
+++ src/Home.java
@@ -5,7 +5,7 @@ public class Home {
 		JFrame frm = new JFrame("Snake Slither bryan");
 		
 		frm.setVisible(true);
-		frm.setSize(500,500);
+		frm.setSize(1000,1000);
 		
 		Game g = new Game();
 		frm.add(g);
