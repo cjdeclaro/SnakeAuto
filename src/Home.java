e57d5f7f4b0c05d4e590bf233de082386806f444
ChanCoronado <coronadochristian55@gmail.com>
2024-07-01 13:41:45 +0800

    Chan

--- src/Home.java
+++ src/Home.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Home {
 	public static void main(String[] args) {
-		JFrame frm = new JFrame("Snake Auto - Chan");
+		JFrame frm = new JFrame("Snake Auto - Chan with daddy chill");
 		
 		frm.setVisible(true);
 		frm.setSize(500,500);
